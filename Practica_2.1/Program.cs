074078608465f9383d603bdbdc1cded046d6a7dc
Charly Perez <Charly121992@outlook.com>
2022-09-12 15:05:23 -0700

    Agregar archivos de proyecto.

---
+++ Practica_2.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica_2._1
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            int edad = 22;
+            string nombre = "Charlie Perez Neri";
+            string pais = "Mexico";
+            
+            string concat = +edad + nombre + pais;
+            Console.WriteLine(concat);
+        }
+    }
+}
